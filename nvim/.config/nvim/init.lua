2ff52dd19880d88db1e0a9be9b783349ffea0270
David Ask <david@formbound.com>
2026-01-20 09:51:35 +0100

    Add zellij

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,2 @@
-require("options")
-require("plugins")
-require("cmds")
-require("autocmds")
-require("mappings")
-require("session")
-require("theme")
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
